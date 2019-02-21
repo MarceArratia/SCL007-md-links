cd0075960f18295b6f71f560946ff874f87717b0
Marce Arratia <marratiaf@hotmail.com>
2019-02-20 21:38:28 -0300

    empezando codigo en index.js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
   // ...
 //};
 const fs = require('fs');
-
+//regexp expresion regular
 fs.readFile('./README.md',function(err,data){
-  if(err){
-    console.log(err);
+  if(RegExp('([a-z0-9_-]+\.){1,2} ',data.toString())){
+    console.log(data.toString());
   }
-  console.log(data);
+  
 });
+//([a-z0-9_-]+\.){1,2} 
