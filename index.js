145ae9092e22ad51895cf33da74d62a83d3459ec
Marce Arratia <marratiaf@hotmail.com>
2019-02-22 11:28:06 -0300

    actualizando codigo para encontrar url en index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 //module.exports = () => {
   // ...
 //};
-let regex = new RegExp("/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi");
+
+let regex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
 const fs = require('fs');
 //regexp expresion regular
 fs.readFile('./README.md',function(err,data){
-  if(data.toString().match(regex) && data.toString().match(regex).indexOf("/")){
     console.log(data.toString().match(regex));
-  }
-  
 });
+/*console.log("Donde esta el archivo");
+var stdin = process.openStdin();//captura lo que usuario escribe
+
+
+stdin.addListener("data", function(d) {//agrego funcion para mostrar lo que usuario escribe
+    console.log("Tu archivo esta en: " + 
+        stdin.toString().trim());
+  });*/
