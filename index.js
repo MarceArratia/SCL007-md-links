ce433e154b0568083516b3d5e4201cdc35a1b9c0
Marce Arratia <marratiaf@hotmail.com>
2019-02-22 09:55:52 -0300

    filtrar url en index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //module.exports = () => {
   // ...
 //};
+let regex = new RegExp("/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi");
 const fs = require('fs');
 //regexp expresion regular
 fs.readFile('./README.md',function(err,data){
-  if(RegExp('([a-z0-9_-]+\.){1,2} ',data.toString())){
-    console.log(data.toString());
+  if(data.toString().match(regex) && data.toString().match(regex).indexOf("/")){
+    console.log(data.toString().match(regex));
   }
   
 });
-//([a-z0-9_-]+\.){1,2} 
