18ec22ddde752756764481daf239d57c797997d3
Marce Arratia <marratiaf@hotmail.com>
2019-02-23 18:23:30 -0300

    corrigiendo filtro de url en archivo index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
   // ...
 //};
 
-let regex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
+let regex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.#?&//=]*)/gi);
 const fs = require('fs');
 //regexp expresion regular
 fs.readFile('./README.md',function(err,data){
     console.log(data.toString().match(regex));
 });
+
 
 //crear funcion md-limks
 
