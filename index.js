0ccd7573724b20e826695c19ba973927dc021811
Marce Arratia <marratiaf@hotmail.com>
2019-02-24 17:53:27 -0300

    usando fetch para encontrar estado de url(ok o error) index.js

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 //module.exports = () => {
   // ...
 //};
-
 let regex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.#?&//=]*)/gi);
 const fs = require('fs');
+const fetch = require("node-fetch");
 //regexp expresion regular
 fs.readFile('./README.md',function(err,data){
-    console.log(data.toString().match(regex));
+    checkUrl(data.toString().match(regex));
+    
 });
+function checkUrl(urlCheck){
+  let url=[];
+  url=String(urlCheck).split(',');  
+  for(let i=0;i<url.length;i++){
+    //console.log(url[i]);
+   fetch(url[i]).then((function(response) {
+     //console.log(response.status);
+     if(response.status === 200){
+      console.log(url[i]+ " Ok");
+     }else{
+      console.log(url[i]+ " Error");
+     }
+    
+  })
+  ).catch(function(error) {
+    console.log('Hubo un problema con la petición Fetch:' + error.message);
+  });
+  }
+  
+
+}
+
+
 
 
 //crear funcion md-limks
